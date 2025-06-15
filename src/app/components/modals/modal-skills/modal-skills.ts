757aa79827ed5a95bd82c8a6abc6b5520c5a0dc3
codariadev <alves2.eduardo@gmail.com>
2025-06-15 17:00:47 -0300

    Implementação do css no skilss

--- src/app/components/modals/modal-skills/modal-skills.ts
+++ src/app/components/modals/modal-skills/modal-skills.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-modal-skills',
   templateUrl: './modal-skills.html',
+  styleUrl: './modal-skills.css',
   imports: [FormsModule, CommonModule],
 })
 export class ModalSkillsComponent {
@@ -19,8 +20,17 @@ export class ModalSkillsComponent {
       this.skill = '';
     }
   }
+  remover(skill: string): void {
+    const index = this.modalService.skills.indexOf(skill);
+    if (index > -1) {
+      this.modalService.skills.splice(index, 1)
+    }
+  }
 
   continuar() {
     this.modalService.avancarEtapa();
+  }
+  voltar() {
+    this.modalService.voltarEtapa();
   }
 }
